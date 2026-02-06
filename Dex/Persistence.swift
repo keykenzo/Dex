10f100bc51a79c77168505bec67bb42378abdc92
Mario Duarte <mariods7@gmail.com>
2026-02-06 15:39:00 -0300

    All item code replaced with Pokemon cod. App runs with erros

--- Dex/Persistence.swift
+++ Dex/Persistence.swift
@@ -19,13 +19,14 @@ struct PersistenceController {
         let viewContext = result.container.viewContext
         
         let newPokemon = Pokemon(context: viewContext)
+        
         newPokemon.id = 1
         newPokemon.name = "bulbasaur"
         newPokemon.types = ["grass", "poison"]
         newPokemon.hp = 45
         newPokemon.attack = 49
         newPokemon.defense = 49
-        newPokemon.specialdefense = 65
+        newPokemon.specialDefense = 65
         newPokemon.speed = 45
         newPokemon.sprite = URL(string:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")
         newPokemon.shiny = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png")
@@ -53,9 +54,10 @@ struct PersistenceController {
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                print(error)
             }
         })
+        container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
