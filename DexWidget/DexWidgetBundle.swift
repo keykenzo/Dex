5ec7bb5a447c19c95a284c9c32554c92dc2d5445
Mario Duarte <mariods7@gmail.com>
2026-02-09 16:34:25 -0300

    created widget extension

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Mario Duarte on 09/02/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
