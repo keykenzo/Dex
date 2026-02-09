1abee201058ff72b4bc6c5eb57536e06cb0250c6
Mario Duarte <mariods7@gmail.com>
2026-02-09 15:29:01 -0300

    stored images func done

--- Dex/ContentView.swift
+++ Dex/ContentView.swift
@@ -66,7 +66,7 @@ struct ContentView: View {
                     Section {
                         ForEach(pokedex) { pokemon in
                             NavigationLink(value: pokemon) {
-                                AsyncImage(url: pokemon.sprite) { image in
+                                AsyncImage(url: pokemon.spriteURL) { image in
                                     image
                                         .resizable()
                                         .scaledToFit()
@@ -179,8 +179,8 @@ struct ContentView: View {
                     pokemon.specialAttack = fetchedPokemon.specialAttack
                     pokemon.specialDefense = fetchedPokemon.specialDefense
                     pokemon.speed = fetchedPokemon.speed
-                    pokemon.sprite = fetchedPokemon.sprite
-                    pokemon.shiny = fetchedPokemon.shiny
+                    pokemon.spriteURL = fetchedPokemon.spriteURL
+                    pokemon.shinyURL = fetchedPokemon.shinyURL
                     
 //                    if pokemon.id % 2 == 0 {
 //                        pokemon.favorite = true
@@ -191,6 +191,22 @@ struct ContentView: View {
                 } catch {
                     print(error)
                 }
+            }
+            storeSprites()
+        }
+    }
+    
+    private func storeSprites() {
+        Task {
+            do {
+                for pokemon in all {
+                    pokemon.sprite = try await URLSession.shared.data(from: pokemon.spriteURL!).0
+                    pokemon.shiny = try await URLSession.shared.data(from: pokemon.shinyURL!).0
+                    try viewContext.save()
+                    print("sprites stores: \(pokemon.id): \(pokemon.name!.capitalized)")
+                }
+            } catch {
+                print(error)
             }
         }
     }
